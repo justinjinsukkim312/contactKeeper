c323e0e8501a89abd234f45370cd9621d349c5e1
Justin Jinsuk Kim <jinsuk@Justins-MacBook-Air.local>
2019-09-06 12:45:27 -0500

    generate jwt

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
+const config = require('config');
+const jwt = require('jsonwebtoken');
 const { check, validationResult } = require('express-validator');
 const User = require('../models/User');
 
@@ -43,8 +45,25 @@ router.post(
 			user.password = await bcrypt.hash(password, salt);
 
 			await user.save();
+			// object to send in token
+			const payload = {
+				user: {
+					id: user.id
+				}
+			};
 
-			res.send('User Saved');
+			// generate a token (payload, secret) expires in an hour (production time)
+			jwt.sign(
+				payload,
+				config.get('jwtSecret'),
+				{
+					expiresIn: 3600
+				},
+				(err, token) => {
+					if (err) throw err;
+					res.json({ token });
+				}
+			);
 		} catch (err) {
 			console.error(err.message);
 			res.status(500).send('Server Error');
