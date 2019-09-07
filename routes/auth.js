bccef94a4c8b6ebe29d938db3e73d8289b8ed8c8
Justin Jinsuk Kim <jinsuk@Justins-MacBook-Air.local>
2019-09-07 15:27:30 -0500

    navbar and router setup, home/about page

--- routes/auth.js
+++ routes/auth.js
@@ -11,13 +11,13 @@ const User = require('../models/User');
 // @desc Get logged in user
 // @access Private
 
-router.get('/', auth, (req, res) => {
-	try{
-		const user = await User.findById(req.user.id).select('-password')
-		res.json(user)
-	} catch(err){
-		console.error(err.message)
-		res.status(500).send('server error')
+router.get('/', auth, async (req, res) => {
+	try {
+		const user = await User.findById(req.user.id).select('-password');
+		res.json(user);
+	} catch (err) {
+		console.error(err.message);
+		res.status(500).send('server error');
 	}
 });
 
