b79e383f86cca2897f633cf1d91c772005456380
Justin Jinsuk Kim <jinsuk@Justins-MacBook-Air.local>
2019-09-11 10:44:42 -0500

    clear form functionality

--- client/src/components/contacts/ContactForm.js
+++ client/src/components/contacts/ContactForm.js
@@ -3,7 +3,7 @@ import ContactContext from '../../context/contact/contactContext';
 
 const ContactForm = () => {
 	const contactContext = useContext(ContactContext);
-	const { addContact, current } = contactContext;
+	const { addContact, current, clearCurrent } = contactContext;
 
 	useEffect(() => {
 		if (current !== null) {
@@ -39,9 +39,14 @@ const ContactForm = () => {
 		});
 	};
 
+	const clearAll = () => {
+		clearCurrent();
+	};
 	return (
 		<form onSubmit={onSubmit}>
-			<h2 className="text-primary">AddContact</h2>
+			<h2 className="text-primary">
+				{current ? 'Edit Contact' : 'Add Contact'}
+			</h2>
 			<input
 				type="text"
 				placeholder="Name"
@@ -83,10 +88,18 @@ const ContactForm = () => {
 			<div>
 				<input
 					type="submit"
-					value="Add Contact"
+					value={current ? 'Update Contact' : 'Add Contact'}
 					className="btn btn-primary btn-block"
 				/>
 			</div>
+			{current && (
+				<div>
+					{' '}
+					<button className="btn btn-light btn-block" onClick={clearAll}>
+						Clear
+					</button>
+				</div>
+			)}
 		</form>
 	);
 };
