c0fe442e35a15826e8e37f5cebc85ff15f099517
Justin Jinsuk Kim <jinsuk@Justins-MacBook-Air.local>
2019-09-13 08:57:20 -0500

    contact filter form without ui updating

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -37,7 +37,8 @@ const ContactState = props => {
 				type: 'professional'
 			}
 		],
-		current: null
+		current: null,
+		filtered: null
 	};
 	const [state, dispatch] = useReducer(contactReducer, initialState);
 
@@ -63,19 +64,26 @@ const ContactState = props => {
 		dispatch({ type: UPDATE_CONTACT, payload: contact });
 	};
 	// filter contacts
-
+	const filterContacts = text => {
+		dispatch({ type: FILTER_CONTACTS, payload: text });
+	};
 	// clear filter
-
+	const clearFilter = () => {
+		dispatch({ type: CLEAR_FILTER });
+	};
 	return (
 		<ContactContext.Provider
 			value={{
 				contacts: state.contacts,
 				current: state.current,
+				filtered: state.filtered,
 				addContact,
 				deleteContact,
 				setCurrent,
 				clearCurrent,
-				updateContact
+				updateContact,
+				filterContacts,
+				clearFilter
 			}}
 		>
 			{props.children}
