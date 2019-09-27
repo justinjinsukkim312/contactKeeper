0a1e82b116b2663eae0d8ade4a9be217bbb9329b
Justin Jinsuk Kim <jinsuk@Justins-MacBook-Air.local>
2019-09-27 13:46:56 -0500

    private route for homepage

--- client/src/App.js
+++ client/src/App.js
@@ -13,6 +13,8 @@ import AuthState from './context/auth/AuthState';
 import AlertState from './context/alert/AlertState';
 import ContactState from './context/contact/ContactState';
 
+import PrivateRoute from './components/routing/PrivateRoute';
+
 import setAuthToken from './utils/setAuthToken';
 
 import './App.css';
@@ -32,7 +34,7 @@ const App = () => {
 							<div className="container">
 								<Alerts />
 								<Switch>
-									<Route exact path="/" component={Home} />
+									<PrivateRoute exact path="/" component={Home} />
 									<Route exact path="/about" component={About} />
 									<Route exact path="/register" component={Register} />
 									<Route exact path="/login" component={Login} />
