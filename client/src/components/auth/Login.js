6250839cf0a6396557d731f924836fbaec43b6b6
Justin Jinsuk Kim <jinsuk@Justins-MacBook-Air.local>
2019-09-27 13:30:45 -0500

    user login

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,6 +1,24 @@
-import React, { useState } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
+import AuthContext from '../../context/auth/authContext';
+import AuthContext from '../../context/alert/alertContext';
 
-const Login = () => {
+const Login = props => {
+	const alertContext = useContext(AlertContext);
+	const authContext = useContext(AuthContext);
+	const { setAlert } = alertContext;
+	const { login, error, clearErrors, isAuthenticated } = authContext;
+
+	useEffect(() => {
+		if (isAuthenticated) {
+			props.history.push('/');
+		}
+		if (error === 'Invalid Credentials') {
+			setAlert(error, 'danger');
+			clearErrors();
+		}
+		// eslint-disable-next-line
+	}, [error, isAuthenticated, props.history]);
+
 	const [user, setUser] = useState({
 		email: '',
 		password: ''
@@ -11,7 +29,14 @@ const Login = () => {
 	const onChange = e => setUser({ ...user, [e.target.name]: e.target.value });
 	const onSubmit = e => {
 		e.preventDefault();
-		console.log('Login submit');
+		if (email === '' || password === '') {
+			setAlert('Please fill in all fields', 'danger');
+		} else {
+			login({
+				email,
+				password
+			});
+		}
 	};
 	return (
 		<div className="form-container">
@@ -30,6 +55,7 @@ const Login = () => {
 						name="password"
 						value={password}
 						onChange={onChange}
+						required
 					/>
 				</div>
 				<input
