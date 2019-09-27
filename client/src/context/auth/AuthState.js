244a5b1d6ae845b917fce98b0be747072fa48ed2
Justin Jinsuk Kim <jinsuk@Justins-MacBook-Air.local>
2019-09-27 13:38:08 -0500

    logout feature

--- client/src/context/auth/AuthState.js
+++ client/src/context/auth/AuthState.js
@@ -80,9 +80,7 @@ const AuthState = props => {
 		}
 	};
 	// logout
-	const logout = () => {
-		console.log('logout');
-	};
+	const logout = () => dispatch({ type: LOGOUT });
 	//clear errors
 	const clearErrors = () => dispatch({ type: CLEAR_ERRORS });
 	return (
