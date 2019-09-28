c127c1e6566ee26ea0a8d86969c3949ade9265da
Justin Jinsuk Kim <jinsuk@Justins-MacBook-Air.local>
2019-09-28 11:46:27 -0500

    update contacts to db

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -58,6 +58,24 @@ const ContactState = props => {
 			dispatch({ type: CONTACT_ERROR, payload: err.response.msg });
 		}
 	};
+	// update contact
+	const updateContact = async contact => {
+		const config = {
+			headers: {
+				'Content-Type': 'application/json'
+			}
+		};
+		try {
+			const res = await axios.put(
+				`/api/contacts/${contact._id}`,
+				contact,
+				config
+			);
+			dispatch({ type: UPDATE_CONTACT, payload: res.data });
+		} catch (err) {
+			dispatch({ type: CONTACT_ERROR, payload: err.response.msg });
+		}
+	};
 	// clear contacts
 	const clearContacts = () => {
 		dispatch({ type: CLEAR_CONTACTS });
@@ -70,10 +88,7 @@ const ContactState = props => {
 	const clearCurrent = () => {
 		dispatch({ type: CLEAR_CURRENT });
 	};
-	// update contact
-	const updateContact = contact => {
-		dispatch({ type: UPDATE_CONTACT, payload: contact });
-	};
+
 	// filter contacts
 	const filterContacts = text => {
 		dispatch({ type: FILTER_CONTACTS, payload: text });
